ef1f9024e3d3ad46118915ae7e8bca8bb774b96f
陈达寅 <11612512@mail.sustc.edu.cn>
2020-01-23 20:44:21 -0800

    Update insertion-sort.cc

--- insertion-sort.cc
+++ insertion-sort.cc
@@ -19,7 +19,7 @@ mySort (int N, keytype* A)
   for(i = 1; i < N; i++){
 	  j = i - 1;
 	  temp = A[i];
-	  while(j >= 0 && A[j] > A[i]){
+	  while(j >= 0 && A[j] > temp){
 		  A[j+1] = A[j];
 		  j--;
 	  }
